0e7c923b29b03c27b7b7bae8a216542b8babed99
Yeabsira Negash <n.yeabsira.a@gmail.com>
2024-03-26 00:25:15 +0000

    sad attempt of extension

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
+
 const Country = ({country, addCountry}) => {
 
-    
+    // const [hasVisited, setHasVisited] = useState(true)
     const mappedCapitals = () => {
         if(country.capital){
             return country.capital.map((capital, id) => <li key={id}>{capital}</li>);
@@ -12,6 +14,13 @@ const Country = ({country, addCountry}) => {
         addCountry(country);
     }
 
+    // const handleClick = (e) => {
+    //     if(hasVisited){
+    //         addCountry(country);
+    //         return <button onClick={handleClick}>Visited!</button>
+    //     }
+    // };
+
     return ( 
         <>
             <h2>{country.name.official}</h2>
@@ -20,6 +29,7 @@ const Country = ({country, addCountry}) => {
                 {mappedCapitals()}
             </ul>
             <button onClick={handleClick}>Visited!</button>
+            {/* <p>{handleClick()}</p> */}
         </>
      );
 }
